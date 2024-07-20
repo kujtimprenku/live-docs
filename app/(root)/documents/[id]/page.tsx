02f30cc62a0dbfcfaefb36129df52edb3d1fa64a
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-07-20 09:37:22 +0200

    Add inital pages and install the editor Added documents/{id} page Added sign-in page Added sign-up page Added simple header Added assets Added simple editor and its styles

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
+
+export default Document;
