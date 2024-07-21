f0d3434855cd2953c7d0a1c5a33dcbc6e83391ea
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-07-21 15:53:14 +0200

    Show all document comments on the right side of the document.

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -20,6 +20,7 @@ import {
 import Loader from "../Loader";
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 import { useThreads } from "@liveblocks/react/suspense";
+import Comments from "../Comments";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -79,6 +80,7 @@ export function Editor({
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
+            <Comments />
           </LiveblocksPlugin>
         </div>
       </div>
